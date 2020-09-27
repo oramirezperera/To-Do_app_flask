f3b503e95c7cd44ad92c4f82757a24420077e216
Orlando Ramirez <ramirez.orlando17@gmail.com>
2020-09-27 18:03:35 -0400

    changes to the readme file and the todo file

--- todo/todo.py
+++ todo/todo.py
@@ -23,10 +23,30 @@ def index():
 @bp.route('/create', methods=['GET', 'POST'])
 @login_required
 def create():
-    return ''
+    if request.method == 'POST':
+        description = request.form['description']
+        error = None
+
+        if not description:
+            error = 'Description required'
+
+        if error is not None:
+            flash(error)
+        else:
+            db, c = get_db()
+            c.execute(
+                'insert into todo (description, completed, created_by)'
+                ' values (%s, %s, %s)',
+                (description, False, g.user['id'])
+            )
+
+            db.commit()
+            return redirect(url_for('todo.index'))
+
+    return render_template('todo/create.html')
 
 
-@bp.route('/update', methods=['GET', 'POST'])
+@bp.route('/<int: id>/update', methods=['GET', 'POST'])
 @login_required
 def update():
-    return ''
+    return render_template('todo/update.html', todo=todo)
